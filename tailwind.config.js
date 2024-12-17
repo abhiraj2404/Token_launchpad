496484515b18f712b8dafed8e99be74cc8a7c085
Abhiraj Singh Chauhan <chauhanabhiraj06@gmail.com>
2024-12-17 17:06:08 +0530

    modify tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,20 @@
-import {nextui} from '@nextui-org/theme'
+import { nextui } from '@nextui-org/theme'
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/components/(avatar|button|card|code|dropdown|image|input|kbd|link|listbox|navbar|select|snippet|spinner|toggle|ripple|menu|divider|popover|scroll-shadow).js",
   ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["var(--font-sans)"],
-        mono: ["var(--font-mono)"],
+        sans: [ "var(--font-sans)" ],
+        mono: [ "var(--font-mono)" ],
       },
     },
   },
   darkMode: "class",
-  plugins: [nextui()],
+  plugins: [ nextui() ],
 }
